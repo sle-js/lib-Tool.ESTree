9168ee8be2a4e5e9362d2cd4c87bacdeb64146b2
Graeme Lockley <graemel@no9.co.za>
2017-10-10 21:13:18 +0200

    refactor: simplify Array.set

--- src/Array.js
+++ src/Array.js
@@ -25,13 +25,10 @@ Array.indexedMap = f => a => {
 // Set the element at a particular index. Returns an updated array. If the index is out of range, the array is
 // unaltered.
 //= set :: Int -> a -> Array a
-Array.set = index => value => a => {
-    return (index >= 0 && index < Array.length(a))
-        ? Array.concat(
-            Array.append(value)(Array.slice(0)(index)(a)))(
-            Array.drop(index + 1)(a))
+Array.set = index => value => a =>
+    (index >= 0 && index < Array.length(a))
+        ? Array.concat(Array.append(value)(Array.slice(0)(index)(a)))(Array.drop(index + 1)(a))
         : a;
-};
 assumptionEqual(Array.set(-1)(9)([0, 1, 2, 3, 4]), [0, 1, 2, 3, 4]);
 assumptionEqual(Array.set(10)(9)([0, 1, 2, 3, 4]), [0, 1, 2, 3, 4]);
 assumptionEqual(Array.set(0)(9)([0, 1, 2, 3, 4]), [9, 1, 2, 3, 4]);
