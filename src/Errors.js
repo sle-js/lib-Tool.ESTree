19af4a1c82a479e7b36f784547907ca2441e7b67
Graeme Lockley <graemel@no9.co.za>
2017-10-15 16:34:04 +0200

    refactor: remove unused tokenExpected error

--- src/Errors.js
+++ src/Errors.js
@@ -7,16 +7,11 @@ const conditionFailed = lexer =>
     new ErrorsType([0, lexer]);
 
 
-const tokenExpected = lexer => token =>
-    new ErrorsType([1, lexer, token]);
-
-
 const orFailed = lexer =>
     new ErrorsType([2, lexer]);
 
 
 module.exports = {
     conditionFailed,
-    orFailed,
-    tokenExpected
+    orFailed
 };
