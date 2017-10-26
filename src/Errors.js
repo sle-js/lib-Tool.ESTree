abbf073b45defb5653b7c7c315f80bf433a10be6
Graeme Lockley <graemel@no9.co.za>
2017-10-26 05:58:10 +0200

    feature: add a Duplicate Identifier error

--- src/Errors.js
+++ src/Errors.js
@@ -7,6 +7,7 @@
 // data Errors =
 //      ExpectedTokens { loc :: Location, found :: { id :: Int, symbol :: String, value :: String }, expected :: Array { id :: Int, symbol :: String } }
 //    | InvalidImport { loc :: Location, url :: String, reason :: String }
+//    | DuplicateIdentifier { locs :: Array Location, name :: String }
 
 
 const Position = line => column =>
@@ -30,10 +31,15 @@ const InvalidImport = loc => url => code =>
     new Errors$({kind: "InvalidImport", loc, url, code});
 
 
+const DuplicateIdentifier = locs => name =>
+    ({kind: "DuplicateIdentifier", locs, name});
+
+
 module.exports = {
     Errors$,
     Location,
     Position,
+    DuplicateIdentifier,
     ExpectedTokens,
     InvalidImport
 };
