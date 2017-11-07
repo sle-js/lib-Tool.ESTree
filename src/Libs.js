6c0187e8bd03f448f6aa43e099675ec1f98891ee
Graeme Lockley <graemel@no9.co.za>
2017-11-07 19:23:02 +0200

    refactor: upgrade to core:Native.Data.Array dependency and remove custom array functions

--- src/Libs.js
+++ src/Libs.js
@@ -1,5 +1,5 @@
 module.exports = {
-    Array: require("./Array"),
+    Array: mrequire("core:Native.Data.Array:1.2.0"),
     Stream: mrequire("core:Data.Collection.InfiniteStream:1.0.0"),
     Maybe: mrequire("core:Native.Data.Maybe:1.0.0"),
     String: require("./String")
