91a01810b59742d1df288a6e0b789e0b30b552ca
Graeme Lockley <graemel@no9.co.za>
2017-11-19 11:44:45 +0200

    refactor: get test Libs into the right shape

--- test/Libs.js
+++ test/Libs.js
@@ -1,15 +1,16 @@
-const Libs = require("../src/Libs");
+module.exports = $importAll([
+    "../src/Libs"
+]).then($imports => {
+    return Object.assign({}, $imports[0], {
+        Assertion: mrequire("core:Test.Unit.Assertion:2.0.1"),
+        Index: require("../index"),
+        LexerConfiguration: require("../src/LexerConfiguration"),
+        Parser: require("../src/Parser"),
+        Path: require("path"),
+        Transform: require("../src/Transform"),
+        Translator: require("../src/Translator"),
+        Validation: require("../src/Validation"),
+        Unit: mrequire("core:Test.Unit:1.0.0")
+    });
 
-
-module.exports = Object.assign({}, Libs, {
-    Assertion: mrequire("core:Test.Unit.Assertion:2.0.1"),
-    Index: require("../index"),
-    LexerConfiguration: require("../src/LexerConfiguration"),
-    Parser: require("../src/Parser"),
-    Path: require("path"),
-    Transform: require("../src/Transform"),
-    Translator: require("../src/Translator"),
-    Validation: require("../src/Validation"),
-    Unit: mrequire("core:Test.Unit:1.0.0")
 });
-
