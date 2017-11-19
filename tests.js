959162df8619d2b5c127f69551a376c319932323
Graeme Lockley <graemel@no9.co.za>
2017-11-19 11:14:38 +0200

    refactor: change into a promise style

--- tests.js
+++ tests.js
@@ -1,11 +1,18 @@
-const Unit = require("./test/Libs").Unit;
+module.exports = $import(
+    "./test/Libs"
+).then($imports => {
+    const Unit = $imports.Unit;
 
-
-Unit.Suite("All")([
-    require("./test/LexerTest"),
-    require("./test/TestRunner"),
-    require("./test/UseESTreeTest")
-])
-    .then(Unit.showDetail)
-    .then(Unit.showSummary)
-    .then(Unit.setExitCodeOnFailures);
+    return Unit.Suite("All")([
+        require("./test/LexerTest"),
+        require("./test/TestRunner"),
+        require("./test/UseESTreeTest")
+    ])
+        .then(Unit.showDetail)
+        .then(Unit.showSummary)
+        .then(Unit.setExitCodeOnFailures);
+}).catch(err => {
+    console.error(err);
+    process.exitCode = -1;
+    return err;
+});
