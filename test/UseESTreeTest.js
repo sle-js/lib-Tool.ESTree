5de39d293b9980d46d2c405a59fd8f43be849f28
Graeme Lockley <graemel@no9.co.za>
2017-11-19 11:37:25 +0200

    refactor: change UseESTreeTest to be promise based

--- test/UseESTreeTest.js
+++ test/UseESTreeTest.js
@@ -1,63 +1,66 @@
-const Array = require("./Libs").Array;
-const Assertion = require("./Libs").Assertion;
-const FileSystem = require("./Libs").FileSystem;
-const Path = require("./Libs").Path;
-const Unit = require("./Libs").Unit;
-
-const Use = require("./Libs").Index;
-
-
-const toString = o =>
-    JSON.stringify(o, null, 2);
+module.exports = $import(
+    "./Libs"
+).then($imports => {
+    const Array = $imports.Array;
+    const Assertion = $imports.Assertion;
+    const FileSystem = $imports.FileSystem;
+    const Path = $imports.Path;
+    const Unit = $imports.Unit;
+    const Use = $imports.Index;
 
 
-const path = relativeName =>
-    Path.join(Path.dirname(__filename), relativeName);
+    const toString = o =>
+        JSON.stringify(o, null, 2);
 
 
-const thenTest = name => promise => thenAssertion =>
-    promise
-        .then(okay => Unit.Test(name)(thenAssertion(okay)))
-        .catch(err => Unit.Test(name)(Assertion.fail("Error handler raised: " + err)));
+    const path = relativeName =>
+        Path.join(Path.dirname(__filename), relativeName);
 
 
-const catchTest = name => promise => errorAssertion =>
-    promise
-        .then(_ => Unit.Test(name)(Assertion.fail(toString(_))))
-        .catch(err => Unit.Test(name)(errorAssertion(err)))
-        .catch(err => Unit.Test(name)(Assertion.fail("Error handler raised: " + err)));
+    const thenTest = name => promise => thenAssertion =>
+        promise
+            .then(okay => Unit.Test(name)(thenAssertion(okay)))
+            .catch(err => Unit.Test(name)(Assertion.fail("Error handler raised: " + err)));
 
 
-assertKindIs = kind => errs =>
-    Assertion.isTrue(Array.all(x => x.kind === kind)(errs));
+    const catchTest = name => promise => errorAssertion =>
+        promise
+            .then(_ => Unit.Test(name)(Assertion.fail(toString(_))))
+            .catch(err => Unit.Test(name)(errorAssertion(err)))
+            .catch(err => Unit.Test(name)(Assertion.fail("Error handler raised: " + err)));
 
 
-module.exports = Unit.Suite("UseESTree")([
-    catchTest("Source file does not exist")(
-        Use.translate("./invalid_file_name_that_does_not_exist"))(
-        assertKindIs("SourceFileNotFound")),
+    assertKindIs = kind => errs =>
+        Assertion.isTrue(Array.all(x => x.kind === kind)(errs));
 
-    catchTest("Source file contains a syntax error")(
-        Use.translate(path("./useestree/001.input")))(
-        assertKindIs("ExpectedTokens")),
 
-    catchTest("Source file contains an invalid import")(
-        Use.translate(path("./useestree/002.input")))(
-        assertKindIs("InvalidImport")),
+    return Unit.Suite("UseESTree")([
+        catchTest("Source file does not exist")(
+            Use.translate("./invalid_file_name_that_does_not_exist"))(
+            assertKindIs("SourceFileNotFound")),
 
-    catchTest("Source file contains a valid import but the imported file has a syntax error")(
-        Use.translate(path("./useestree/003.input")))(
-        assertKindIs("ExpectedTokens")),
+        catchTest("Source file contains a syntax error")(
+            Use.translate(path("./useestree/001.input")))(
+            assertKindIs("ExpectedTokens")),
 
-    catchTest("Source file contain validation errors")(
-        Use.translate(path("./useestree/004.input")))(
-        assertKindIs("BaseUnknownDeclaration")),
+        catchTest("Source file contains an invalid import")(
+            Use.translate(path("./useestree/002.input")))(
+            assertKindIs("InvalidImport")),
 
-    thenTest("Translate estree AST")(
-        Use.translate(path("./useestree/005.estree"))
-            .then(result => Promise.all([
-                FileSystem.readFile(path("./useestree/005.js")),
-                FileSystem.readFile(path("./useestree/005.expected.js"))
-            ])))(
-        result => Assertion.equals(result[0])(result[1]))
-]);
+        catchTest("Source file contains a valid import but the imported file has a syntax error")(
+            Use.translate(path("./useestree/003.input")))(
+            assertKindIs("ExpectedTokens")),
+
+        catchTest("Source file contain validation errors")(
+            Use.translate(path("./useestree/004.input")))(
+            assertKindIs("BaseUnknownDeclaration")),
+
+        thenTest("Translate estree AST")(
+            Use.translate(path("./useestree/005.estree"))
+                .then(result => Promise.all([
+                    FileSystem.readFile(path("./useestree/005.js")),
+                    FileSystem.readFile(path("./useestree/005.expected.js"))
+                ])))(
+            result => Assertion.equals(result[0])(result[1]))
+    ]);
+});
