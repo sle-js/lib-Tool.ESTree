8f9c9bc4afd90c2119f6273b2ccd457d261315e0
Graeme Lockley <graemel@no9.co.za>
2017-11-19 11:47:54 +0200

    refactor: load test Libs in parallel using promises

--- test/Libs.js
+++ test/Libs.js
@@ -1,16 +1,24 @@
 module.exports = $importAll([
-    "../src/Libs"
+    "../src/Libs",
+    "core:Test.Unit.Assertion:2.0.1",
+    "../index",
+    "../src/LexerConfiguration",
+    "../src/Parser",
+    "path",
+    "../src/Transform",
+    "../src/Translator",
+    "../src/Validation",
+    "core:Test.Unit:1.0.0"
 ]).then($imports => {
     return Object.assign({}, $imports[0], {
-        Assertion: mrequire("core:Test.Unit.Assertion:2.0.1"),
-        Index: require("../index"),
-        LexerConfiguration: require("../src/LexerConfiguration"),
-        Parser: require("../src/Parser"),
-        Path: require("path"),
-        Transform: require("../src/Transform"),
-        Translator: require("../src/Translator"),
-        Validation: require("../src/Validation"),
-        Unit: mrequire("core:Test.Unit:1.0.0")
+        Assertion: $imports[1],
+        Index: $imports[2],
+        LexerConfiguration: $imports[3],
+        Parser: $imports[4],
+        Path: $imports[5],
+        Transform: $imports[6],
+        Translator: $imports[7],
+        Validation: $imports[8],
+        Unit: $imports[9]
     });
-
 });
